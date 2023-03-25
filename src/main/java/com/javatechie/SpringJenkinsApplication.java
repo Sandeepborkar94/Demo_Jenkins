c5f05f0d2df16cf136232b6bd5f228a73a824346
Sandeep Borkar <39898380+Sandeepborkar94@users.noreply.github.com>
2023-03-25 13:51:33 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/SpringJenkinsApplication.java
@@ -0,0 +1,33 @@
+package com.javatechie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinsApplication 
+{
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void intt() 
+	{
+		logger.info("Application Started......");
+	}
+	
+	
+	public static void main(String[] args) 
+	{
+		logger.info("Application Executing......");
+		
+		logger.error("Application Executing......");
+		
+		logger.warn("Application Executing......");
+		
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
